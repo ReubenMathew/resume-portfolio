97a13265b8ce423305961659a60115fca679db52
Reuben Mathew <intelphysic@gmail.com>
2020-05-01 20:15:48 -0400

    comma list error

--- src/components/Projects/ProjectCard.vue
+++ src/components/Projects/ProjectCard.vue
@@ -8,9 +8,6 @@
                 class="inline-block text-gray-550 print:text-black font-normal group-hover:text-gray-700 transition duration-100 ease-in">↗</span>
             </a>
         </h3>
-        <!-- <p class="leading-normal text-md text-gray-650">
-            Found on GitHub
-        </p> -->
         </header>
         <p class="mt-2 text-md text-gray-700 leading-normal">
             {{description}}
@@ -70,7 +67,7 @@ export default {
             
         }, created () {
             this.getDescription(),
-            this.getDescriptionFromBackend()
+            this.getDescriptionFromBackend(),
             this.getTopics(),
             this.getDescriptionFromBackend()
     },
